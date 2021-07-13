fe2f3935f005e6ff5f1f25d3d21955c2d303899c
Barnbarn96 <59836739+Barnbarn96@users.noreply.github.com>
2021-07-13 16:39:27 +0800

    First Commit

---
+++ 03-frontend/Angular_frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa15oemyc9hu7g175d7',
+        issuer: 'https://dev-83258750.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
