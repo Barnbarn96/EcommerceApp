d2ecb17b31dd650842a6466852956fe529a69182
Barnbarn96 <59836739+Barnbarn96@users.noreply.github.com>
2021-07-18 01:58:14 +0800

    Changed login uri

--- 03-frontend/Angular_frontend/src/app/config/my-app-config.ts
+++ 03-frontend/Angular_frontend/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa15oemyc9hu7g175d7',
         issuer: 'https://dev-83258750.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://barnbarn96.github.io/EcommerceApp/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
