f4cd5ca75f87fcdc90dfd7b95c63e96682b105cc
Barnbarn96 <59836739+Barnbarn96@users.noreply.github.com>
2021-07-21 17:04:24 +0800

    -callback uri

--- 03-frontend/Angular_frontend/src/app/config/my-app-config.ts
+++ 03-frontend/Angular_frontend/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa15oemyc9hu7g175d7',
         issuer: 'https://dev-83258750.okta.com/oauth2/default',
-        redirectUri: 'https://barnbarn96.github.io/EcommerceApp/login/callback',
+        redirectUri: 'https://barnbarn96.github.io/EcommerceApp/login',
         scopes: ['openid', 'profile', 'email']
     }
 }
